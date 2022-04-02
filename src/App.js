ba715b4acc69d554b2f4d6aefa0aa0f78ed4f33c
yumeyuniko <daigocafe@msn.com>
2022-04-02 19:17:44 +0900

    building the UI

--- src/App.js
+++ src/App.js
@@ -14,7 +14,35 @@ function App() {
     return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords());
   }
 
-  return <div className="App">{JSON.stringify(words)}</div>;
+  return (
+    <div className="App">
+      <div className="section">
+        <div className="is-size-1 has-text-centered has-text-primary">
+          <h2>60</h2>
+        </div>
+      </div>
+      <div className="control is-expanded section">
+        <input type="text" className="input" />
+      </div>
+      <div className="section">
+        <button className="button is-info is-fullwidth"> start</button>
+      </div>
+      <div className="section">
+        <div className="card">
+          <div className="card-content">
+            <div className="content">
+              {words.map((word, i) => (
+                <>
+                  <span key={i}>{word}</span>
+                  <span> </span>
+                </>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
